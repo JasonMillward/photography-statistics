c0a968d1d0a7abdfed789edeabd88175a442a0c6
Jason <jason@jcode.me>
2014-03-26 15:54:30 +1030

    Removing copypasta docstring

--- stats.py
+++ stats.py
@@ -1,45 +1,15 @@
 """
-Auto Ripper
-
-Ripping
-    Uses MakeMKV to watch for movies inserted into DVD/BD Drives
-
-    Automaticly checks for existing directory/movie and will NOT overwrite existing
-    files or folders
-
-    Checks minimum length of video to ensure movie is ripped not previews or other
-    junk that happens to be on the DVD
-
-    DVD goes in > MakeMKV gets a proper DVD name > MakeMKV Rips
-    DVD does not get ejected, maybe it will get added to later versions
-
-Compressing
-    An optional additional used to rename and compress movies to an acceptable standard
-    which still delivers quallity audio and video but reduces the file size
-    dramatly.
-
-    Using a nice value of 15 by default, it runs HandBrake as a background task
-    that allows other critical tasks to complete first.
 
 
 Released under the MIT license
 Copyright (c) 2014, Jason Millward
 
 @category   misc
-@version    $Id: 1.6, 2014-03-11 16:52:00 CST $;
 @author     Jason Millward <jason@jcode.me>
 @license    http://opensource.org/licenses/MIT
 
 Usage:
     stats.py <EXIF Attr>... [--dir=<dir>]
-
-Options:
-    -h --help     Show this screen.
-    --version     Show version.
-    --debug       Output debug.
-    --rip         Rip disc using makeMKV.
-    --compress    Compress using handbrake.
-    --test        Testing?
 
 """
 
